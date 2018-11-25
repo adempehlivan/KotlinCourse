515a5538a605d2c443ee4d4f8c63c616694f93ec
abc <abc@example.com>
2018-11-25 12:41:46 +0300

    BooleanCharString

--- src/DegiskenKavramiveCesitleri/BoolenCharString.kt
+++ src/DegiskenKavramiveCesitleri/BoolenCharString.kt
@@ -7,4 +7,11 @@ fun main(args: Array<String>) {
     println(isFalse)
     var bool  : Boolean = 5<4
     println(bool)
+
+    var a : Char = 'a'
+    println(a)
+
+    var name : String = "Adem"
+    println("my name is $name \n${name.length}")
+    println(name[0])
 }
