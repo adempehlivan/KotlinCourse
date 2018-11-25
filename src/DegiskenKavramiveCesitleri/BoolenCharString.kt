ed6b10d53ccc9a08aac337f5896313de95417b1c
abc <abc@example.com>
2018-11-25 12:27:49 +0300

    KotlinCourse

---
+++ src/DegiskenKavramiveCesitleri/BoolenCharString.kt
@@ -0,0 +1,10 @@
+package DegiskenKavramiveCesitleri
+
+fun main(args: Array<String>) {
+    var isTrue = true
+    println(isTrue)
+    var isFalse : Boolean = false
+    println(isFalse)
+    var bool  : Boolean = 5<4
+    println(bool)
+}
---
+++ src/DegiskenKavramiveCesitleri/NumberDegiskenTipi.kt
@@ -0,0 +1,14 @@
+package DegiskenKavramiveCesitleri
+
+fun main(args: Array<String>) {
+    println("Merhaba Dünya")
+    //number variables
+    //byte, short, int, long, float, double
+    var number1 : Int = 5
+    println(number1)
+
+    var number2: Byte = Byte.MIN_VALUE
+    var number3: Byte = Byte.MAX_VALUE
+    println("Min value of byte $number2, Max value of byte $number3")
+
+}
